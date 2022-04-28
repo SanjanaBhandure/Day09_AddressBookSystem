918351e4a504cadbb8a51d2f8e17e73121f9124b
SanjanaBhandure <sweetybhandure@gmail.com>
2022-04-28 10:11:06 +0530

    Added: Refactor to add multiple Address Book to the System. Each Address Book has a unique Name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,26 +3,19 @@
 * displays the Contacts in Address Book that can be modified in certain ways.
 *
 * @author: Sanjana Bhandure
-* @version: 1.6
+* @version: 1.7
 * @date: 27-04-2022
 */
 
 package com.Bridgelabz.Day09_AddressBookSystem;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookMain {
-    public static Logger logger = Logger.getLogger(AddressBookMain.class);
     static boolean isRunning = true;
 
     //ArrayList object is created
-    Collection<Contacts> person = new ArrayList<Contacts>();
+    List<Contacts> person = new ArrayList<Contacts>();
     Dictionary address = new Hashtable();
 
     //Taking input from the user
@@ -32,15 +25,15 @@ public class AddressBookMain {
      * console method create for taking input as per the User choice.
      */
     public void console() {
-        logger.info("-----------------");
-        logger.info("1. Create contact");
-        logger.info("2. Edit Contact");
-        logger.info("3. Delete Contact");
-        logger.info("4. Print all contacts");
-        logger.info("5. Add multiple person to Address Book");
-        logger.info("6. Create another address book");
-        logger.info("7. Exit");
-        logger.info("Enter your choice: ");
+        System.out.println("-----------------");
+        System.out.println("1. Create contact");
+        System.out.println("2. Edit Contact");
+        System.out.println("3. Delete Contact");
+        System.out.println("4. Print all contacts");
+        System.out.println("5. Add multiple person to Address Book");
+        System.out.println("6. Create another address book");
+        System.out.println("7. Exit");
+        System.out.println("Enter your choice: ");
 
         int option = scanner.nextInt();
             switch (option) {
@@ -69,12 +62,12 @@ public class AddressBookMain {
                     break;
 
                 case 7:
-                    logger.info("Exit...");
+                    System.out.println("Exit...");
                     isRunning = false;
                     break;
 
                 default:
-                    logger.info("Enter valid option: ");
+                    System.out.println("Enter valid option: ");
                     break;
         }
     }
@@ -86,48 +79,63 @@ public class AddressBookMain {
         Contacts contacts = new Contacts();
 
         Scanner scanner = new Scanner(System.in);
-        logger.info("Enter First Name: ");
+        System.out.println("Enter First Name: ");
         String firstName = scanner.nextLine();
         contacts.setFirstName(firstName);
 
-        logger.info("Enter Last Name: ");
+        System.out.println("Enter Last Name: ");
         String lastName = scanner.nextLine();
         contacts.setLastName(lastName);
 
-        logger.info("Enter Address: ");
+        System.out.println("Enter Address: ");
         String address = scanner.nextLine();
         contacts.setAddress(address);
 
-        logger.info("Enter City: ");
+        System.out.println("Enter City: ");
         String city = scanner.nextLine();
         contacts.setCity(city);
 
-        logger.info("Enter State: ");
+        System.out.println("Enter State: ");
         String state = scanner.nextLine();
         contacts.setState(state);
 
-        logger.info("Enter zip: ");
+        System.out.println("Enter zip: ");
         String zip = scanner.nextLine();
         contacts.setZip(zip);
 
-        logger.info("Enter PhoneNumber: ");
+        System.out.println("Enter PhoneNumber: ");
         String phoneNumber = scanner.nextLine();
         contacts.setPhoneNumber(phoneNumber);
 
-        logger.info("Enter Email: ");
+        System.out.println("Enter Email: ");
         String email = scanner.nextLine();
         contacts.setEmail(email);
 
-        person.add(contacts);
-        logger.info("Contact " +firstName+ "created Successfully");
-        System.out.println(person.toString());
+        //Checks whether the contact with the same number is already existing in your Address Book.
+        boolean isDuplicate = false;
+        for (Contacts contact : person) {
+            if (contact.getPhoneNumber().equals(contacts.getPhoneNumber())) {
+                isDuplicate = true;
+            }
+        }
+        //if the contact is already stored in Address book it will print message print.
+        if (isDuplicate)
+        {
+            System.out.println("Contact existing in your Address Book");
+        } else
+        //if not then it will add the contact in your Address Book
+        {
+            person.add(contacts);
+            System.out.println("Contact " +firstName+ " created Successfully");
+            System.out.println(person.toString());
+        }
     }
 
     /*
      * editContacts method created to update contacts with the given fields.
      */
     public void editContacts() {
-        logger.info("Enter the name to edit: ");
+        System.out.println("Enter the name to edit: ");
         String first = scanner.next();
 
         boolean isPerson = false;
@@ -137,81 +145,81 @@ public class AddressBookMain {
             if (((ArrayList<Contacts>) person).get(i).getFirstName().equals(first)) {
 
                 isPerson = true;
-                logger.info("Enter your choice to edit: 1. First Name\n2. Last Name\n3. Address\n4.City\n" +
+                System.out.println("Enter your choice to edit: 1. First Name\n2. Last Name\n3. Address\n4.City\n" +
                         "5. State\n6. Zip\n7. PhoneNumber\n8. Email\n9. Exit ");
                 int choice = scanner.nextInt();
                 switch (choice) {
                     case 1:
-                        logger.info("Enter First Name: ");
+                        System.out.println("Enter First Name: ");
                         String firstName = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setFirstName(firstName);
                         break;
 
                     case 2:
-                        logger.info("Enter Last Name: ");
+                        System.out.println("Enter Last Name: ");
                         String lastName = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setLastName(lastName);
                         break;
 
                     case 3:
-                        logger.info("Enter Address: ");
+                        System.out.println("Enter Address: ");
                         String address = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setAddress(address);
 
                     case 4:
-                        logger.info("Enter City: ");
+                        System.out.println("Enter City: ");
                         String city = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setCity(city);
                         break;
 
                     case 5:
-                        logger.info("Enter State: ");
+                        System.out.println("Enter State: ");
                         String state = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setState(state);
                         break;
 
                     case 6:
-                        logger.info("Enter Zip: ");
+                        System.out.println("Enter Zip: ");
                         String zip = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setZip(zip);
                         break;
 
                     case 7:
-                        logger.info("Enter PhoneNumber: ");
+                        System.out.println("Enter PhoneNumber: ");
                         String phoneNumber = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setPhoneNumber(phoneNumber);
                         break;
 
                     case 8:
-                        logger.info("Enter Email: ");
+                        System.out.println("Enter Email: ");
                         String email = scanner.next();
                         ((ArrayList<Contacts>)person).get(i).setEmail(email);
                         break;
 
                     default:
-                        logger.info("Select valid option: ");
+                        System.out.println("Select valid option: ");
 
                 } //end of switch
-                logger.info(person);
+                System.out.println(person);
             }
         }
         if (!isPerson)
-            logger.info("Contact not found!!!");
+            System.out.println("Contact not found!!!");
     } // end of for loop
 
     /*
      * deleteContacts method created to remove specific contact.
      */
     public void deleteContact() {
-        logger.info("Enter the First Name of the contact that you want to delete: ");
+        System.out.println("Enter the First Name of the contact that you want to delete: ");
         String firstName = scanner.next();
 
         for (int i = 0; i< person.size();i++) {
             if (((ArrayList<Contacts>) person).get(i).getFirstName().equals(firstName)) {
                 person.remove(((ArrayList<Contacts>)person).get(i));
-                logger.info("Contact removed successfully!!!");
+                System.out.println("Contact removed successfully!!!");
             } else
-                logger.info("Contact not found.");
+                System.out.println("Contact not found.");
         }
     }
 
@@ -220,7 +228,7 @@ public class AddressBookMain {
      */
     public void addMultiplePerson() {
         while (true) {
-            logger.info("Enter your choice \n1. Create Contact\n2. Edit Contact" + "\n3. To Delete Contact\n4. Exit");
+            System.out.println("Enter your choice \n1. Create Contact\n2. Edit Contact" + "\n3. To Delete Contact\n4. Exit");
             int option = scanner.nextInt();
             switch (option) {
                 case 1 -> createContacts();
@@ -239,27 +247,26 @@ public class AddressBookMain {
      */
     public void addAddressBook() {
         Scanner scanner = new Scanner(System.in);
-        logger.info("Enter name of address Book you want: ");
+        System.out.println("Enter name of address Book you want: ");
         AddressBookMain addressBookMain = new AddressBookMain();
         String bookName = scanner.nextLine();
         address.put(bookName,addressBookMain);
-        logger.info("Address Book " +bookName+ " has been created.");
+        System.out.println("Address Book " +bookName+ " has been created.");
     }
     /*
      * printAllContacts method created to print all contacts of AddressBook
      */
     public void printAllContacts() {
         for (int i = 0; i < person.size();i++) {
-            logger.info(((ArrayList<Contacts>)person).get(i));
+            System.out.println(((ArrayList<Contacts>)person).get(i));
         }
     }
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
         /*
          * Displaying Welcome to Address Book Program in AddressBookMain class
          */
-        logger.info("Welcome to Address Book Program!");
+        System.out.println("Welcome to Address Book Program!");
         //AddressBookMain object created
         AddressBookMain addressBookMain = new AddressBookMain();
         while (isRunning) {
